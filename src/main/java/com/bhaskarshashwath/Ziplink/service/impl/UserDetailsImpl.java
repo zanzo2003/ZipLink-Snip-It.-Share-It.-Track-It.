3f222fe384495dd000a6c8c127f2a0521dd45baf
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-07-27 21:48:25 +0530

    feat: spring secruity UserDetails Implementation

---
+++ src/main/java/com/bhaskarshashwath/Ziplink/service/impl/UserDetailsImpl.java
@@ -0,0 +1,73 @@
+package com.bhaskarshashwath.Ziplink.service.impl;
+
+import com.bhaskarshashwath.Ziplink.domain.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+/*
+ This class implements Spring Security's UserDetails interface to provide user-specific information
+ for authentication and authorization. It allows storing user details in the SecurityContext,
+ eliminating the need to repeatedly fetch user data when accessing protected resources.
+*/
+
+@Data
+@NoArgsConstructor
+@Service
+public class UserDetailsImpl implements UserDetails {
+
+    private static final Long serialVersion = 1L;
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String password, String email, Collection<? extends GrantedAuthority> authorities){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
+    /*
+    build method to create Spring Security UserDetails from ZipLink User class
+     */
+
+    public static UserDetailsImpl build(User user){
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getEmail(),
+                Collections.singletonList(authority));
+    }
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+
+
+    @Override
+    public String getUsername(){
+        return this.username;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return this.authorities;
+    }
+
+}
