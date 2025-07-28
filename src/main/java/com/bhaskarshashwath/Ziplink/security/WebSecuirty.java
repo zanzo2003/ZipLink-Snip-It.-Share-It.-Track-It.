616dc0208f34f49d11dd5a9c21e05d808da4dc1b
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-07-28 22:24:18 +0530

    feat: Jwt utils class with methods

---
+++ src/main/java/com/bhaskarshashwath/Ziplink/security/WebSecuirty.java
@@ -0,0 +1,4 @@
+package com.bhaskarshashwath.Ziplink.security;
+
+public class WebSecuirty {
+}
---
+++ src/main/java/com/bhaskarshashwath/Ziplink/security/jwt/JwtUtils.java
@@ -0,0 +1,82 @@
+package com.bhaskarshashwath.Ziplink.security.jwt;
+
+import com.bhaskarshashwath.Ziplink.service.impl.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+
+@Slf4j
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    public String getJwtFromAuthHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+
+    public String generateToken(UserDetailsImpl userDeatils){
+        String username = userDeatils.getUsername();
+        String roles = userDeatils.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date( (new Date().getTime() + this.jwtExpirationMs)))
+                .signWith(key())
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token){
+        try {
+            return Jwts.parser()
+                    .verifyWith((SecretKey) key())
+                    .build().parseSignedClaims(token)
+                    .getPayload().getSubject();
+        }catch (Exception e){
+            log.error("Error exteacting subject : ", e.getStackTrace());
+            return null;
+        }
+    }
+
+    public Boolean validateToken(String token){
+        try {
+            Jwts.parser()
+                    .verifyWith((SecretKey) key())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            log.error("Error validation token : ", e.getStackTrace());
+            return false;
+        }
+    }
+
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(this.jwtSecret));
+    }
+}
