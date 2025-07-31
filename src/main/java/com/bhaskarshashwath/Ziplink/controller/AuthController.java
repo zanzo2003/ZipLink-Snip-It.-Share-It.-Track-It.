f6eb46f28dcec484f1bb126ba1ce760cab974704
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-07-31 23:39:51 +0530

    feat: custom exception for handling username not unique

--- src/main/java/com/bhaskarshashwath/Ziplink/controller/AuthController.java
+++ src/main/java/com/bhaskarshashwath/Ziplink/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.bhaskarshashwath.Ziplink.controller;
 
 
 import com.bhaskarshashwath.Ziplink.controller.common.ControllerHelper;
+import com.bhaskarshashwath.Ziplink.domain.User;
 import com.bhaskarshashwath.Ziplink.model.UserDTO;
 import com.bhaskarshashwath.Ziplink.model.common.ApiResponseDTO;
+import com.bhaskarshashwath.Ziplink.service.impl.UserDetailsServiceImpl;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +20,23 @@ public class AuthController {
 
 
     @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
     private ControllerHelper controllerHelper;
 
     @PostMapping("/public/register")
-    public ResponseEntity<ApiResponseDTO> registerUser(
+    public ResponseEntity<ApiResponseDTO<UserDTO>> registerUser(
             @RequestBody @NonNull UserDTO newUserDetails
             ){
 
-
+        User newUser = new User();
+        newUser.setUsername(newUserDetails.getUsername());
+        newUser.setEmail(newUserDetails.getEmail());
+        newUser.setPassword(newUserDetails.getPassword());
+        newUser.setRole("ROLE_USER");
+        userDetailsService.createUser(newUser);
+        return controllerHelper.createOkResponse(newUserDetails, "registration successful");
     }
 
 }
---
+++ src/main/java/com/bhaskarshashwath/Ziplink/exception/GlobalExceptionhandler.java
@@ -0,0 +1,39 @@
+package com.bhaskarshashwath.Ziplink.exception;
+
+
+import com.bhaskarshashwath.Ziplink.controller.common.ControllerHelper;
+import com.bhaskarshashwath.Ziplink.model.common.ApiResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionhandler {
+
+    @Autowired
+    private ControllerHelper controllerHelper;
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponseDTO<Object>> handleIllegalArgument(IllegalArgumentException exception){
+        return controllerHelper.createErrorResponse(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundExcpetion.class)
+    public ResponseEntity<ApiResponseDTO<Object>> handleResourcenotFound(ResourceNotFoundExcpetion exception){
+        return controllerHelper.createErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<ApiResponseDTO<Object>> usernameNotUnique(UsernameAlreadyExistsException exception){
+        return controllerHelper.createErrorResponse(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDTO<Object>> handleGenericException(Exception exception) {
+        return controllerHelper.createErrorResponse(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
